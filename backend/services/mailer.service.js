1c0ae1d50fadbb34218f7276fc52246769147b9b
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-08-23 19:04:39 +0200

    fixed dotenv import to get data from env

--- backend/services/mailer.service.js
+++ backend/services/mailer.service.js
@@ -1,24 +1,16 @@
 import nodemailer from 'nodemailer'
+import 'dotenv/config'
 
 // costruisco il transporter, l'oggetto che nodemailer mi da per costruire 
 // un collegamento col server smtp per l'invio delle mail
-// const transport = nodemailer.createTransport({
-//     host: process.env.MAILTRAP_HOST,
-//     port: process.env.MAILTRAP_PORT,
-//     auth: {
-//         user: process.env.MAILTRAP_USER,
-//         pass: process.env.MAILTRAP_PASS
-//     }
-// });
-
-// sembra che non funzioni recuperando le variabili dall'env
 const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
     auth: {
-        user: "04ab41d6c61a41",
-        pass: "d0db9e6342bc6b"
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS
     }
 });
+
 
 export default transport
